e8a03e487a4a4b2e1cbbb6cffce6fc2f47781f58
dhiliph naren <112474259+DhiliphNaren@users.noreply.github.com>
2022-09-01 14:38:50 +0530

    Add files via upload

---
+++ Dhiliph Naren.py
@@ -0,0 +1,18 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+lsit1=[1,'22AAI001',9]
+list1
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    list1
+NameError: name 'list1' is not defined. Did you mean: 'lsit1'?
+list1=[1,'22AAI001',9]
+list1
+[1, '22AAI001', 9]
+type(list1)
+<class 'list'>
+list2=[2.0,3.0,1.0]
+list2
+[2.0, 3.0, 1.0]
+type(list2)
+<class 'list'>
